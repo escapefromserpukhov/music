d88a0fb15f234d5313dfb947875f0f9a5486a08a
Viktoria_finch <85229737+escapefromserpukhov@users.noreply.github.com>
2022-08-13 08:50:21 +0300

    Create 3.sql

---
+++ 3.sql
@@ -0,0 +1,5 @@
+insert into album (album_name,year_of_release)
+values ('Альбом 2019', 2019), ('MISS', 2020)
+
+insert into  track (track_name, dlitelnost, album_id)
+values ('Младшая сестра', 120, 10), ('Помни меня',100,9)
